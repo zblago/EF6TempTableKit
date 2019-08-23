4587dee9b849974b1812201b858ba1c0eaa85ed8
zblago <blzoran@gmail.com>
2019-08-23 23:09:15 +0200

    Get all fields, db types mapped on field, positions

--- SharDev.EFInterceptor/Extensions/DbContextExtensions.cs
+++ SharDev.EFInterceptor/Extensions/DbContextExtensions.cs
@@ -1,8 +1,9 @@
 ﻿using SharDev.EFInterceptor.DbContext;
 using SharDev.EFInterceptor.Model;
+using SharDev.EFInterceptor.SqlUtility;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SharDev.EFInterceptor.Extensions
 {
@@ -21,19 +22,32 @@ namespace SharDev.EFInterceptor.Extensions
             public static T WithTempExpression<T>(this DbContextInterceptor dbContextExtended, IQueryable<ITempTable> expression)
                 where T : class
             {
-                var sql = expression.ToTraceQuery();
-
-                var objectQuery = expression.GetObjectQuery();
-
-                var positions = objectQuery.GetQueryPropertyPositions();
-
                 var tempTableType = expression.ElementType.FullName;
                 if (dbContextExtended.TempSqlQueriesList.ContainsKey(tempTableType))
                 {
-                    throw new Exception("temp table key alred there");
+                    throw new Exception("temp table key already there");
                 }
 
-                dbContextExtended.InsertTempExpressions(tempTableType, sql);
+                var tempTableExpressionsql = expression.ToTraceQuery();
+                var objectQuery = expression.GetObjectQuery();
+                var positions = objectQuery.GetQueryPropertyPositions();
+
+                var tempTableNameFromConstructor = expression.ElementType.BaseType.CustomAttributes.Single(cs => cs.AttributeType == typeof(TableAttribute)).ConstructorArguments[0].Value;
+                var tempTableNameFromArguments = expression.ElementType.BaseType.CustomAttributes.Single(cs => cs.AttributeType == typeof(TableAttribute)).NamedArguments.Where(n => n.MemberName == nameof(TableAttribute.Name));
+
+                var fields = expression.ElementType.BaseType.GetProperties().Where(p => p.CustomAttributes.Any(ca => ca.AttributeType == typeof (Attributes.TempFieldTypeAttribute)));
+                var fieldName = fields.Select(f => new
+                    {
+                        Name = f.Name,
+                        FieldType = f.CustomAttributes.Single(ca => ca.AttributeType == typeof(Attributes.TempFieldTypeAttribute))
+                            .ConstructorArguments.Single().Value
+                });
+
+
+                var tempTableCreator = new TempTableCreator();
+                //tempTableCreator.CreateTempTable("").AddInsertQuery();
+
+                dbContextExtended.InsertTempExpressions(tempTableType, tempTableExpressionsql);
                  
                 return dbContextExtended as T;
             }
