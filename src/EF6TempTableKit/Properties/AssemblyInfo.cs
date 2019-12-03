132a1ac52b71d5a72d019d97912f162e5e164f20
zblago <blzoran@gmail.com>
2019-12-03 22:33:46 +0100

    Separate branch for 1.0.0.0

--- src/EF6TempTableKit/Properties/AssemblyInfo.cs
+++ src/EF6TempTableKit/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EF6TempTableKit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extends EF6 with temp tables")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zoran Blagojevic")]
 [assembly: AssemblyProduct("EF6TempTableKit")]
