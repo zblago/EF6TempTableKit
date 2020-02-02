a695bdefa407a7c9cd06c526e5959f95f12d1eab
zblago <blzoran@gmail.com>
2020-02-02 19:58:35 +0100

    EF-0001 - AppendSqlString renamed AppendIfNotAlreadyAttached as it's extended with alreadyAttachedTempTable check. More tests.

--- tests/EF6TempTableKit.Test/UtilityTest.cs
+++ tests/EF6TempTableKit.Test/UtilityTest.cs
@@ -42,112 +42,20 @@ namespace EF6TempTableKit.Test
         private AdventureWorksCodeFirst CreateTempQueries()
         {
             var context = new AdventureWorksCodeFirst();
-            
-            #region Create temp tables
 
-            var tempAddressQuery = context.Addresses.Select(a => new AddressTempTableDto { Id = a.AddressID, Name = a.AddressLine1 });
-
-            var tempPartTypeQuery = context.Addresses.Select(a => new PartTypeTempTableDto { PartTypeId = a.AddressID, Name = a.AddressLine1 });
-
-            var tempManufacturerQuery = context.Addresses
-                .Join(context.TempAddresses,
-                    (a) => a.AddressID,
-                    (ta) => ta.Id,
-                    (a, ta) => new ManufacturerTempTableDto { Id = a.AddressID, AddressId = ta.Id, Name = a.AddressLine1 });
-
-            var tempPartQuery = context.Addresses
-                .Join(context.TempManufacturers,
-                    (p) => p.AddressID,
-                    (tm) => tm.AddressId,
-                    (p, tm) => new
-                    {
-                        AddressId = p.AddressID,
-                        Id = tm.Id,
-                        Name = tm.Name,
-                    })
-                .Join(context.TempPartTypes,
-                    (a) => a.Id,
-                    (tmp) => tmp.PartTypeId,
-                    (a, tmp) => new PartTempTableDto
-                    {
-                        PartId = a.AddressId,
-                        PartTypeId = tmp.PartTypeId,
-                        Name = tmp.Name,
-                        ManufacturerId = a.Id
-                    });
-
-            var tempChairQuery = context.Addresses
-                    .Join(context.TempParts,
-                        (a) => a.AddressID,
-                        (tp) => tp.PartId,
-                        (a, tp) => new ChairTempTableDto
-                        {
-                            ChairId = a.AddressID,
-                            PartId = tp.PartId,
-                            Name = a.AddressLine1
-                        });
-
-            var tempRoomQuery = context.Addresses
-                .Join(context.TempChairs,
-                    (a) => a.AddressID,
-                    (tc) => tc.ChairId,
-                    (a, tc) => new RoomTempTableDto
-                    {
-                        RoomId = a.AddressID,
-                        Name = a.AddressLine1,
-                        ChairId = tc.ChairId
-                    });
-
-            var tempOfficeTypeQuery = context.Addresses.Select(tot => new OfficeTypeTempTableDto { Id = tot.AddressID, Name = tot.AddressLine1 });
-
-            var tempOfficeQuery = context.Addresses
-                .Join(context.TempRooms,
-                    (a) => a.AddressID,
-                    (to) => to.RoomId,
-                    (a, to) => new OfficeTempTableDto
-                    {
-                        Id = a.AddressID,
-                        RoomId = to.RoomId,
-                        Name = a.AddressLine1,
-                        OfficeTypeId = context.TempOfficeTypes.FirstOrDefault().Id,
-                        DepartmentId = context.TempDepartments.FirstOrDefault().DepartmentId
-                    });
-
-            var tempPersonQuery = context.Addresses
-                .Join(context.TempAddresses,
-                    (a) => a.AddressID,
-                    (ta) => ta.Id,
-                    (a, ta) => new PersonTempTableDto
-                    {
-                        PersonId = a.AddressID,
-                        Name = a.AddressLine2,
-                        AddressId = ta.Id
-                    });
-
-            var tempDepartmentQuery = context.Addresses
-                .Join(context.TempPersons,
-                    (a) => a.AddressID,
-                    (tp) => tp.AddressId,
-                    (a, tp) => new DepartmentTempTableDto
-                    {
-                        DepartmentId = a.AddressID,
-                        Name = a.AddressLine1,
-                        LeadId = tp.PersonId
-                    });
-
-            #endregion
+            var queryModel = new QueryModel(context);
 
             //Attach temp tables
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempOfficeTypeQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempAddressQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempManufacturerQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempPartTypeQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempPartQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempChairQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempRoomQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempPersonQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempDepartmentQuery);
-            context.WithTempTableExpression<AdventureWorksCodeFirst>(tempOfficeQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempOfficeTypeQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempAddressQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempManufacturerQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPartTypeQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPartQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempChairQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempRoomQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPersonQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempDepartmentQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempOfficeQuery);
 
             return context;
         }
@@ -340,6 +248,40 @@ namespace EF6TempTableKit.Test
                         T = context.TempAddresses.FirstOrDefault().Id
                     }).ToList();
             }
+        }
+
+        [Fact]
+        public void TempOnTempDependenciesInQueryOrderChanged()
+        {
+            var context = new AdventureWorksCodeFirst();
+            var queryModel = new QueryModel(context);
+
+            //Attach temp tables
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempOfficeTypeQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempAddressQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPersonQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempDepartmentQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempManufacturerQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPartTypeQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempPartQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempChairQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempRoomQuery);
+            context.WithTempTableExpression<AdventureWorksCodeFirst>(queryModel.TempOfficeQuery);
+
+            var test = context.TempAddresses.Select(a => new
+            {
+                id1 = context.TempOffices.FirstOrDefault().Id,
+                id2 = context.TempAddresses.FirstOrDefault().Id,
+                id3 = context.TempManufacturers.FirstOrDefault().Id,
+                id4 = context.TempPartTypes.FirstOrDefault().PartTypeId,
+                id5 = context.TempChairs.FirstOrDefault().ChairId,
+                id6 = context.TempRooms.FirstOrDefault().RoomId,
+                id7 = context.TempPersons.FirstOrDefault().PersonId,
+                id8 = context.TempDepartments.FirstOrDefault().DepartmentId,
+                id9 = context.TempOffices.FirstOrDefault().Id,
+            }).ToList();
+
+            Assert.NotEmpty(test);
         }
     }
 }
