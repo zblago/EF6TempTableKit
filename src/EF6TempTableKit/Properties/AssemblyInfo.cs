b39b80278fd9de5192cd66ee2c32288fe3765e20
Zoran Blagojevic <zblagojevic@neogov.net>
2023-08-02 17:34:53 +0200

    EF-0018 -update assembly version and readme file

--- src/EF6TempTableKit/Properties/AssemblyInfo.cs
+++ src/EF6TempTableKit/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.0.*")] //File version
-[assembly: AssemblyInformationalVersion("3.0.0.0")] //Product version
+[assembly: AssemblyInformationalVersion("3.1.0.0")] //Product version
 
 #if DEBUG
 [assembly: InternalsVisibleTo("EF6TempTableKit.Test")]
